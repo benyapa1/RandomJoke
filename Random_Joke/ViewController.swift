1822488df75f353560b7a4445a2512247077be40
Papada Preedagorn <papadapdg@MacBook-Pro-khxng-Papada.local>
2019-08-08 14:15:45 +0700

    initial commit

---
+++ Random_Joke/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Random_Joke
+//
+//  Created by Papada Preedagorn on 8/8/2562 BE.
+//  Copyright © 2562 Papada Preedagorn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
